53d44dc1ef2dd8441b1b5d17dc48ec968fecc2c4
RaJharit77 <hei.rajo.2@gmail.com>
2024-12-16 09:58:49 +0300

    chore: Updating about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,15 +6,17 @@ const About = () => {
             <h2 className="text-4xl font-bold mb-6" data-aos="fade-up">À Propos de Moi</h2>
             <h3 className="text-2xl font-semibold mb-6 text-blue-500" data-aos="fade-up">Je vais me présenter...</h3>
             <p className="text-lg max-w-2xl" data-aos="fade-up">
-                Bonjour! Je m'appelle Rajoharitiana, un développeur de logiciels créatif et passionné, basé à Madagascar. 
-                J'ai un amour profond pour le codage et j'apprécie le plaisir d'apprendre de nouvelles choses. Avec 
-                une expérience acquise durant ma deuxième année de licence (L2), je suis motivé pour créer des applications web et mobiles.
+                Bonjour ! Je m'appelle Rajoharitiana, un développeur front-end créatif et passionné basé à Madagascar. Je suis spécialisé dans la conception et le développement d'interfaces utilisateur modernes et intuitives,
+                offrant des expériences engageantes et réactives.
+                Avec une solide expérience acquise au cours de mes études en licence (L2) et divers projets personnels,
+                j'ai développé des compétences dans des technologies front-end telles que React.js, Tailwind CSS, et bien d'autres.
                 <br /><br />
-                Je crois que la meilleure façon de grandir est de constamment repousser mes limites et d'explorer de nouvelles technologies. 
-                Qu'il s'agisse de développer des interfaces utilisateur élégantes ou de construire des systèmes back-end robustes, 
-                je suis toujours prêt à relever de nouveaux défis et à contribuer à des projets innovants.
+                Mon objectif est de transformer des idées en interfaces fonctionnelles et esthétiques tout en assurant une expérience utilisateur optimale.
+                Je crois fermement que l'apprentissage continu est essentiel pour évoluer dans ce domaine en constante évolution.
+                J'adore explorer de nouvelles technologies, repousser mes limites et collaborer sur des projets innovants.
                 <br /><br />
-                En dehors du codage, j'aime aussi jouer aux échecs, lire des livres sur la technologie et passer du temps en plein air.
+                En dehors du développement, je me passionne pour les échecs, la lecture d'ouvrages sur la technologie et les balades en plein air, 
+                qui m'aident à trouver l'inspiration et à rester motivé.
             </p>
         </section>
     );
