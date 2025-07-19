453c937d3ac2d87eac98db0fe24cdbc76e2e7190
RaJharit77 <hei.rajo.2@gmail.com>
2025-07-19 14:34:35 +0300

    chore: delete AOS init in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,5 +10,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
-
-AOS.init();
